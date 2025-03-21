e0efc26b3b147b1f890032a4a46106df2b2afcfc
Aslan Dossymzhan <dossymzhanaslan@gmail.com>
2025-03-21 14:35:28 +0500

    added job to server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
 
-import { connectDB } from "./lib/db.js";
 
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes)
