9ef584f1c35e0043279621d7acc3e23408a48bad
Aslan Dossymzhan <dossymzhanaslan@gmail.com>
2025-03-21 06:56:57 +0500

    removed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,7 +11,7 @@ const generateToken = (userId) => {
 router.post("/register", async (req, res) => {
     try{
         const { username, email, password } = req.body;
-
+        
         if(!username || !email || !password) return res.status(400).json({message: "All fields are required"});
         
 
